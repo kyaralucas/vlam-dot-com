fe49ed4296f6a51da23f673424806b456d63e3b4
boredkyara <kyaralucas@live.com>
2020-02-19 22:54:24 +0100

    Minor changes

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@
 	///////////////////////////
 	// Preloader
 	$(window).on('load', function() {
-		$("#preloader").delay(600).fadeOut();
+		$("#preloader").delay(400).fadeOut();
 	});
 
 	///////////////////////////
@@ -29,6 +29,7 @@
 			scrollTop: 0
 		}, 600);
 	});
+
 
 	///////////////////////////
 	// Btn nav collapse
